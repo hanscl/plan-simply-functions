24c483cae2a1c614d4e5a6e444efb018e8fc5f5a
hanscl <hans@Hanss-MacBook-Pro-2.local>
2020-09-17 21:13:21 -0700

    removed driver additions from master branch for clean deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,3 @@ export { planVersionGroupCreate } from "./plan_version_group_calc";
 export { planViewGenerate } from "./plan_view_generate";
 export { planVersionRecalc } from "./plan_version_recalc";
 export { entityHierarchyUpdate } from "./entity_update_hier";
-
-// Driver-based
-export { driverDefinitionCreate } from "./driver_def_create"
