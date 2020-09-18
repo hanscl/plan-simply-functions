83ce4eb5ab2b90a771b2bf45d27e50094681ed08
hanscl <hans@Hanss-MacBook-Pro-2.local>
2020-09-17 20:27:32 -0700

    remove deprecated plan and planview functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,6 @@
 // Security
 export { writeUserAccount } from "./custom_claims";
 
-//export { updateAccountRollups } from './account_update';
-//export { updatePlanViews } from './planview_recalc';
-//export { planViewCreate } from "./plan_view_create";
-//export { planViewFullCalc } from "./plan_view_fullcalc";
-//export { planViewReCalc } from "./plan_view_recalc";
-
 // NEW VERSIONS (GEAMS TESTING)
 export { planVersionGroupCreate } from "./plan_version_group_calc";
 export { planViewGenerate } from "./plan_view_generate";
