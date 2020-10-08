f985c6564b66e60d0993670a9f40a2421dc38080
hanscl <hans@Hanss-MacBook-Pro-2.local>
2020-10-08 01:13:59 -0700

    247-add-labor-positions >> complete

--- functions/src/labor_entry_update.ts
+++ functions/src/labor_entry_update.ts
@@ -55,18 +55,13 @@ export const laborEntryUpdate = functions.firestore
           pos_doc_before.acct === pos_doc_after.acct &&
           pos_doc_before.dept === pos_doc_after.dept &&
           pos_doc_before.fte_factor === pos_doc_after.fte_factor &&
-          JSON.stringify(pos_doc_before.ftes?.values) ===
-            JSON.stringify(pos_doc_after.ftes?.values) &&
+          JSON.stringify(pos_doc_before.ftes?.values) === JSON.stringify(pos_doc_after.ftes?.values) &&
           pos_doc_before.pos === pos_doc_after.pos &&
           pos_doc_before.status === pos_doc_after.status
         ) {
           if (
-            (pos_doc_after.status === "Hourly" &&
-              pos_doc_before.rate !== undefined &&
-              pos_doc_before.rate.hourly === pos_doc_after.rate.hourly) ||
-            (pos_doc_after.status === "Salary" &&
-              pos_doc_before.rate !== undefined &&
-              pos_doc_before.rate.annual === pos_doc_after.rate.annual)
+            (pos_doc_after.status === "Hourly" && pos_doc_before.rate !== undefined && pos_doc_before.rate.hourly === pos_doc_after.rate.hourly) ||
+            (pos_doc_after.status === "Salary" && pos_doc_before.rate !== undefined && pos_doc_before.rate.annual === pos_doc_after.rate.annual)
           ) {
             console.log(`All input values are unchanged. Exit`);
             return;
@@ -75,32 +70,15 @@ export const laborEntryUpdate = functions.firestore
       }
 
       // Save plan id ...
-      const labor_snap = await db
-        .doc(
-          `entities/${context_params.entity_id}/labor/${context_params.version_id}`
-        )
-        .get();
-      if (!labor_snap.exists)
-        throw new Error(
-          `Could not get labor doc with version id ${context_params.version_id}. Fatal Error.`
-        );
+      const labor_snap = await db.doc(`entities/${context_params.entity_id}/labor/${context_params.version_id}`).get();
+      if (!labor_snap.exists) throw new Error(`Could not get labor doc with version id ${context_params.version_id}. Fatal Error.`);
       context_params.plan_id = (labor_snap.data() as labor_model.laborVersionDoc).plan_id;
       // ... and also get the plan document
-      const plan_snap = await db
-        .doc(
-          `entities/${context_params.entity_id}/plans/${context_params.plan_id}`
-        )
-        .get();
-      if (!plan_snap.exists)
-        throw new Error(
-          `Plan document ${context_params.plan_id} does not exists for entity ${context_params.entity_id}. Exiting.`
-        );
+      const plan_snap = await db.doc(`entities/${context_params.entity_id}/plans/${context_params.plan_id}`).get();
+      if (!plan_snap.exists) throw new Error(`Plan document ${context_params.plan_id} does not exists for entity ${context_params.entity_id}. Exiting.`);
       const plan_doc = plan_snap.data() as plan_model.planDoc;
 
-      const days_in_months = utils.getDaysInMonth(
-        plan_doc.begin_year,
-        plan_doc.begin_month
-      );
+      const days_in_months = utils.getDaysInMonth(plan_doc.begin_year, plan_doc.begin_month);
       console.log(`Days in plan months are: ${JSON.stringify(days_in_months)}`);
 
       calculateWages(pos_doc_after, days_in_months);
@@ -108,33 +86,44 @@ export const laborEntryUpdate = functions.firestore
       calculateRate(pos_doc_after);
 
       // write updated document
-      await labor_snap.ref
-        .collection("positions")
-        .doc(context_params.position_id)
-        .update(pos_doc_after);
+      await labor_snap.ref.collection("positions").doc(context_params.position_id).update(pos_doc_after);
 
       // now update the gl account => ONLY if we have values for ACCT and DEPT
-      if (
-        pos_doc_after.acct !== undefined &&
-        pos_doc_after.dept !== undefined
-      ) {
+      if (pos_doc_after.acct !== undefined && pos_doc_after.dept !== undefined) {
         await recalcGlAccount(context_params, pos_doc_after);
         // ... also if either ACCT/DEPT (or both) changed, then we need to recalculate the combination where the position was removed from
-        if (
-          pos_doc_before.acct !== pos_doc_after.acct ||
-          pos_doc_before.dept !== pos_doc_after.dept
-        )
+        if (pos_doc_before.acct !== pos_doc_after.acct || pos_doc_before.dept !== pos_doc_after.dept)
           await recalcGlAccount(context_params, pos_doc_before);
+      }
+
+      // If there are no more blank positions, add one more
+      // TODO: Remove when we have add additional labor positions in the UI
+      const pos_snap = await db
+        .collection(`entities/${context_params.entity_id}/labor/${context_params.version_id}/positions`)
+        .where("acct", "==", "")
+        .where("dept", "==", "")
+        .where("pos", "==", "")
+        .get();
+
+      if (pos_snap.empty) {
+        const empty_labor_pos: labor_model.positionDoc = {
+          pos: "",
+          acct: "",
+          dept: "",
+          fte_factor: 0,
+          ftes: { total: 0, values: utils.getValuesArray() },
+          rate: { annual: 0, hourly: 0 },
+          status: "Hourly",
+        };
+        await db.collection(`entities/${context_params.entity_id}/labor/${context_params.version_id}/positions`).add(empty_labor_pos);
+      
       }
     } catch (error) {
       console.log(`Error occured while processing labor update: ${error}`);
     }
   });
 
-function calculateWages(
-  position: labor_model.positionDoc,
-  days_in_months: number[]
-) {
+function calculateWages(position: labor_model.positionDoc, days_in_months: number[]) {
   if (position.wages === undefined) {
     position.wages = { total: 0, values: utils.getValuesArray() };
   }
@@ -156,46 +145,27 @@ function calculateWages(
   position.wages.total = 0;
   for (let mnth_idx = 0; mnth_idx < 12; mnth_idx++) {
     if (position.status === "Salary" && position.rate.annual !== undefined) {
-      position.wages.values[mnth_idx] =
-        (days_in_months[mnth_idx] / days_in_year) *
-        position.ftes.values[mnth_idx] *
-        position.rate.annual;
+      position.wages.values[mnth_idx] = (days_in_months[mnth_idx] / days_in_year) * position.ftes.values[mnth_idx] * position.rate.annual;
       position.wages.total += position.wages.values[mnth_idx];
-      position.wages.values[mnth_idx] = utils.finRound(
-        position.wages.values[mnth_idx]
-      );
+      position.wages.values[mnth_idx] = utils.finRound(position.wages.values[mnth_idx]);
       100;
-    } else if (
-      position.status === "Hourly" &&
-      position.fte_factor !== undefined &&
-      position.rate.hourly !== undefined
-    ) {
-      position.wages.values[mnth_idx] =
-        days_in_months[mnth_idx] *
-        (position.fte_factor / 52 / 7) *
-        position.ftes.values[mnth_idx] *
-        position.rate.hourly;
+    } else if (position.status === "Hourly" && position.fte_factor !== undefined && position.rate.hourly !== undefined) {
+      position.wages.values[mnth_idx] = days_in_months[mnth_idx] * (position.fte_factor / 52 / 7) * position.ftes.values[mnth_idx] * position.rate.hourly;
       position.wages.total += position.wages.values[mnth_idx];
-      position.wages.values[mnth_idx] = utils.finRound(
-        position.wages.values[mnth_idx]
-      );
+      position.wages.values[mnth_idx] = utils.finRound(position.wages.values[mnth_idx]);
     }
   }
   position.wages.total = utils.finRound(position.wages.total);
 }
 
-function calculateAvgFTEs(
-  days_in_months: number[],
-  ftes: labor_model.laborCalc
-) {
+function calculateAvgFTEs(days_in_months: number[], ftes: labor_model.laborCalc) {
   const days_in_year = days_in_months.reduce((a, b) => {
     return a + b;
   }, 0);
 
   let avg_ftes = 0;
   for (let mnth_idx = 0; mnth_idx < 12; mnth_idx++) {
-    avg_ftes +=
-      ftes.values[mnth_idx] * (days_in_months[mnth_idx] / days_in_year);
+    avg_ftes += ftes.values[mnth_idx] * (days_in_months[mnth_idx] / days_in_year);
   }
 
   ftes.total = utils.finRound(avg_ftes);
@@ -205,28 +175,16 @@ function calculateRate(position: labor_model.positionDoc) {
   if (position.rate === undefined || position.fte_factor === undefined) return;
 
   if (position.status === "Hourly" && position.rate.hourly !== undefined) {
-    position.rate.annual = utils.finRound(
-      position.rate.hourly * position.fte_factor
-    );
-  } else if (
-    position.status === "Salary" &&
-    position.rate.annual !== undefined
-  ) {
-    position.rate.hourly = utils.finRound(
-      position.rate.annual / position.fte_factor
-    );
+    position.rate.annual = utils.finRound(position.rate.hourly * position.fte_factor);
+  } else if (position.status === "Salary" && position.rate.annual !== undefined) {
+    position.rate.hourly = utils.finRound(position.rate.annual / position.fte_factor);
   }
 }
 
-async function recalcGlAccount(
-  context_params: contextParams,
-  updated_position: labor_model.positionDoc
-) {
+async function recalcGlAccount(context_params: contextParams, updated_position: labor_model.positionDoc) {
   // get all positions accounts for the same dept and acct
   const pos_snap = await db
-    .collection(
-      `entities/${context_params.entity_id}/labor/${context_params.version_id}/positions`
-    )
+    .collection(`entities/${context_params.entity_id}/labor/${context_params.version_id}/positions`)
     .where("acct", "==", updated_position.acct)
     .where("dept", "==", updated_position.dept)
     .get();
@@ -245,9 +203,7 @@ async function recalcGlAccount(
 
   // update the account doc
   const acct_snap = await db
-    .collection(
-      `entities/${context_params.entity_id}/plans/${context_params.plan_id}/versions/${context_params.version_id}/dept`
-    )
+    .collection(`entities/${context_params.entity_id}/plans/${context_params.plan_id}/versions/${context_params.version_id}/dept`)
     .where("acct", "==", updated_position.acct)
     .where("dept", "==", updated_position.dept)
     .get();
