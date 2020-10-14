ec6dd0acd24ccd9a2a9e4d1da6e930acaed094e8
hanscl <hans@Hanss-MacBook-Pro-2.local>
2020-10-13 20:29:14 -0700

    293-daily-backup-prod-data >> done but not deploying

--- functions/src/firestore_backup.ts
+++ functions/src/firestore_backup.ts
@@ -1,33 +1,36 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 import * as key from "../generator-zerobase-d888b3abeba3.json";
 
 import { google } from "googleapis";
 
-
 const authClient = new google.auth.JWT({
-    email: key.client_email,
-    key: key.private_key,
-    scopes: ["https://www.googleapis.com/auth/datastore", "https://www.googleapis.com/auth/cloud-platform"]
+  email: key.client_email,
+  key: key.private_key,
+  scopes: ["https://www.googleapis.com/auth/datastore", "https://www.googleapis.com/auth/cloud-platform"],
 });
 
 const firestoreClient = google.firestore({
-    version: "v1beta2",
-    auth: authClient
+  version: "v1beta2",
+  auth: authClient,
 });
 
-exports.backupFirestore = functions.pubsub.schedule('every day 00:00').onRun(async (context) => {
-    const projectId = process.env.GCP_PROJECT
+exports.backupFirestore = functions.pubsub
+  .schedule("0 21 * * *")
+  .timeZone("America/Los_Angeles")
+  .onRun(async (context) => {
+    const projectId = process.env.GCP_PROJECT;
 
-    const timestamp = new Date().toISOString()
+    if (projectId !== "generator-zerobase") return;
 
-    console.log(`Start to backup project ${projectId}`)
+    const timestamp = new Date().toISOString();
+
+    console.log(`Start to backup project ${projectId}`);
 
     await authClient.authorize();
     return firestoreClient.projects.databases.exportDocuments({
-        name: `projects/${projectId}/databases/(default)`,
-        requestBody: {
-            outputUriPrefix: `gs://${projectId}-firestore-backups/backups/${timestamp}`
-        }
-    })
-
-});
+      name: `projects/${projectId}/databases/(default)`,
+      requestBody: {
+        outputUriPrefix: `gs://${projectId}-firestore-backups/backups/${timestamp}`,
+      },
+    });
+  });
