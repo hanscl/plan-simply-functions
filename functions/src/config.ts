41071abd44891b3e25ec4ac5df2a9d57cc46328c
hanscl <hans@Hanss-MacBook-Pro-2.local>
2020-11-09 15:36:09 -0800

    change cf location config

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
-export const cloudFuncLoc = functions.config().app.env === 'prod' ? 'europe-west1' : 'us-central1';
+export const cloudFuncLoc = functions.config().app.env === 'prod' ? 'us-central1' : 'us-central1';
 
 export function getProjectId(): string | undefined {
     const fb_config = process.env.FIREBASE_CONFIG;
