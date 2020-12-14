20a3266d0ae1c4e6b49e7bd85d3c88f602e42a06
hanscl <hans@Hanss-MacBook-Pro-2.local>
2020-12-13 17:09:11 -0800

    remove deprecated version calculation :x:

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,6 @@ export { writeUserAccount } from "./custom_claims";
 // NEW VERSIONS
 export { planVersionGroupCreate } from "./plan_version_group_calc"; 
 export { planViewGenerate } from "./plan_view_generate";
-// export { planVersionRecalc } from "./plan_version_recalc";
 export { entityHierarchyUpdate } from "./entity_update_hier";
 export { versionDocCreate } from "./version_create";
 export { createVersionFromExisting } from "./https_duplicate_version";
