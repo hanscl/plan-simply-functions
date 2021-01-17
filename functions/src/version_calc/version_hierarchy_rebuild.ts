766170af7b7d64d1b665761f0cebf2177729a3c4
hanscl <hans@Hanss-MacBook-Pro-2.local>
2021-01-16 21:32:35 -0800

    initial completion of functionality, pending testing

--- functions/src/version_calc/version_hierarchy_rebuild.ts
+++ functions/src/version_calc/version_hierarchy_rebuild.ts
@@ -7,7 +7,7 @@ import * as utils from '../utils';
 // import * as viewModel from '../view_model';
 // import * as driverModel from '../driver_model';
 
-// import { getEntityDetails, getVersionDetails } from './version_calc_helpers';
+import { getEntityDetails } from './version_calc_helpers';
 
 import { CalcRequest } from './version_calc_model';
 const db = admin.firestore();
@@ -21,7 +21,7 @@ const rebuildVersionHierarchy = async (calcRequest: CalcRequest) => {
     await versionDocumentReference.update({ calculated: false });
 
     // get additional information from the database for the recalc process
-    // const entity = await getEntityDetails(db, calcRequest.entityId);
+    const entity = await getEntityDetails(db, calcRequest.entityId);
     // const version = await getVersionDetails(db, calcRequest);
 
     // delete all rollups => pnl coll / div coll /
@@ -50,7 +50,10 @@ const rebuildVersionHierarchy = async (calcRequest: CalcRequest) => {
 
       for (const rollupDocument of rollupDocSnapshot.docs) {
         const rollupData = rollupDocument.data() as entityModel.EntityRollupDocument;
-        processRollupLevel(rollupData, divDict, deptDict, acctDict, rollupSummary.items, versionDocumentReference);
+        processRollupLevel(rollupData, divDict, deptDict, acctDict, rollupSummary.items, versionDocumentReference, [
+          entity.full_account,
+          entity.div_account,
+        ]);
       }
     }
 
@@ -67,7 +70,8 @@ const processRollupLevel = async (
   deptDict: entityModel.deptDict,
   acctDict: entityModel.acctDict,
   rollupNames: entityModel.rollupNameMap[],
-  versionDocRef: FirebaseFirestore.DocumentReference
+  versionDocRef: FirebaseFirestore.DocumentReference,
+  entityFullAccountStrings: string[]
 ) => {
   console.log(`processing rollup definition: ${JSON.stringify(rollupDefinition)}`);
 
@@ -108,6 +112,137 @@ const processRollupLevel = async (
       acctList.push(childAccount);
     }
   }
+  if (acctList.length > 0) {
+    updateAllAccounts(
+      acctList,
+      rollupDefinition,
+      divDict,
+      deptDict,
+      acctDict,
+      rollupNames,
+      versionDocRef,
+      entityFullAccountStrings
+    );
+  }
+};
+
+const updateAllAccounts = async (
+  acctList: planModel.accountDoc[],
+  rollupDefinition: entityModel.EntityRollupDocument,
+  divDict: entityModel.divDict,
+  deptDict: entityModel.deptDict,
+  acctDict: entityModel.acctDict,
+  rollupNames: entityModel.rollupNameMap[],
+  versionDocRef: FirebaseFirestore.DocumentReference,
+  entityFullAccountStrings: string[]
+) => {
+  for (const divId of Object.keys(divDict)) {
+    let divAccountsFound = false;
+    const divAccountsUpdated: string[] = [];
+
+    for (const deptId of divDict[divId].depts) {
+      let deptAccountsFound = false;
+      const acctsForDept = acctList.filter((acct) => acct.dept === deptId);
+
+      for (const account of acctsForDept) {
+        divAccountsFound = true;
+        deptAccountsFound = true;
+        let acctName = '';
+
+        if (account.class === 'acct') {
+          acctName = acctDict[account.acct].name;
+        } else {
+          acctName = rollupNames.filter((nameMap) => nameMap.code === account.acct)[0].name;
+        }
+
+        await versionDocRef
+          .collection('dept')
+          .doc(account.full_account)
+          .update({
+            parent_rollup: account.parent_rollup,
+            acct_name: acctName,
+            total: account.total,
+            divdept_name: deptDict[account.dept ? account.dept : ''].name,
+            is_group_child: false,
+          });
+
+        if (account.class === 'rollup' && !divAccountsUpdated.includes(account.acct)) {
+          const divRollupQuerySnap = await versionDocRef
+            .collection('div')
+            .where('acct', '==', account.acct)
+            .where('div', '==', divId)
+            .get();
+          if (!divRollupQuerySnap.empty) {
+            await divRollupQuerySnap.docs[0].ref.update({ parent_rollup: account.parent_rollup });
+            divAccountsUpdated.push(account.acct);
+          }
+        }
+      }
+      if (deptAccountsFound) {
+        await updateRollupAccountsInFirestore(
+          divDict,
+          deptDict,
+          rollupNames,
+          versionDocRef,
+          entityFullAccountStrings,
+          rollupDefinition.rollup,
+          divId,
+          deptId
+        );
+      }
+    } // end dept loop
+    if (divAccountsFound) {
+      await updateRollupAccountsInFirestore(
+        divDict,
+        deptDict,
+        rollupNames,
+        versionDocRef,
+        entityFullAccountStrings,
+        rollupDefinition.rollup,
+        divId
+      );
+    }
+  } // end div loop
+};
+
+const updateRollupAccountsInFirestore = async (
+  divDict: entityModel.divDict,
+  deptDict: entityModel.deptDict,
+  rollupNames: entityModel.rollupNameMap[],
+  versionDocRef: FirebaseFirestore.DocumentReference,
+  entityFullAccountStrings: string[],
+  acctId: string,
+  divId: string,
+  deptId?: string
+) => {
+  const fullAccount = utils.buildFullAccountString(entityFullAccountStrings, {
+    acct: acctId,
+    div: divId,
+    dept: deptId,
+  });
+  const acctUpdates = {
+    acct: acctId,
+    acct_name: rollupNames.filter((rollup) => rollup.code === acctId)[0].name,
+    class: 'rollup',
+    group: false,
+    is_group_child: false,
+    div: divId,
+    total: 0,
+    values: utils.getValuesArray(),
+    full_account: fullAccount,
+  };
+
+  let finalAcctUpdates = {};
+  if (deptId) {
+    finalAcctUpdates = { ...acctUpdates, divdept_name: deptDict[deptId].name, dept: deptId };
+  } else {
+    finalAcctUpdates = { ...acctUpdates, divdept_name: divDict[divId].name };
+  }
+
+  await versionDocRef
+    .collection(deptId ? deptId : divId)
+    .doc(fullAccount)
+    .set(finalAcctUpdates);
 };
 
 const getEntityStructureData = async (
