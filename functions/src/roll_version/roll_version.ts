e02b32904ab6f49dbc0e8e6cedf0d2777645ed38
hanscl <hans@Hanss-MacBook-Pro-2.local>
2021-01-20 15:45:00 -0800

    create API endpoint, required interfaces and function body for wrapper

---
+++ functions/src/roll_version/roll_version.ts
@@ -0,0 +1,12 @@
+import * as admin from 'firebase-admin';
+
+import { RollVersionRequest } from './roll_version_model';
+
+const db = admin.firestore();
+
+export const beginRollVersion = async (rollVersionRequest: RollVersionRequest) => {
+  try {
+  } catch (error) {
+    throw new Error(`Error occured in [beginRollVersion]: ${error}`);
+  }
+};
